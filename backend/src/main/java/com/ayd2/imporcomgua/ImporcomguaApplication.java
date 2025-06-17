7a9d373de13b71827502064cf058a2f1371375eb
CCRR1000x2 <jaquelinecifuentes201130968@cunoc.edu.gt>
2025-06-17 16:56:57 -0600

    chore: initial backend project structure

---
+++ backend/src/main/java/com/ayd2/imporcomgua/ImporcomguaApplication.java
@@ -0,0 +1,13 @@
+package com.ayd2.imporcomgua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImporcomguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImporcomguaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ayd2/imporcomgua/ImporcomguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayd2.imporcomgua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImporcomguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
