70f6273b73526c5439cb4453c8fa71da223d64cb
LuisCif-20 <n.rodas225@gmail.com>
2025-06-19 18:42:50 -0600

    fix: location repositories

--- backend/src/main/java/com/ayd2/imporcomgua/repositories/location/DepartmentRepository.java
+++ backend/src/main/java/com/ayd2/imporcomgua/repositories/location/DepartmentRepository.java
@@ -1,13 +1,11 @@
 package com.ayd2.imporcomgua.repositories.location;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ayd2.imporcomgua.models.location.Department;
 
 public interface DepartmentRepository extends JpaRepository<Department, String> {
-
-    Optional<Department> findByCode(String code);
+    
+    boolean existByCode(String code);
     
 }
