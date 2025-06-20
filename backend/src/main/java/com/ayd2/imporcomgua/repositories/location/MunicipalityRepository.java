bffce912b5eb89076a41c5745cf2cc123fe77a70
LuisCif-20 <n.rodas225@gmail.com>
2025-06-20 00:30:31 -0600

    feat: add client repository

--- backend/src/main/java/com/ayd2/imporcomgua/repositories/location/MunicipalityRepository.java
+++ backend/src/main/java/com/ayd2/imporcomgua/repositories/location/MunicipalityRepository.java
@@ -1,10 +1,14 @@
 package com.ayd2.imporcomgua.repositories.location;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ayd2.imporcomgua.models.location.Municipality;
 
 public interface MunicipalityRepository extends JpaRepository<Municipality, String>, JpaSpecificationExecutor<Municipality> {
+
+    Optional<Municipality> findByCode(String code);
 
 }
