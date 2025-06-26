46925159adf6ef9be6cfb2d0b60911e938252633
LuisCif-20 <n.rodas225@gmail.com>
2025-06-26 12:54:38 -0600

    feat: add business crud

--- backend/src/main/java/com/ayd2/imporcomgua/controllers/client/BusinessController.java
+++ backend/src/main/java/com/ayd2/imporcomgua/controllers/client/BusinessController.java
@@ -1,16 +1,26 @@
 package com.ayd2.imporcomgua.controllers.client;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ayd2.imporcomgua.dto.client.BusinessResponseDTO;
+import com.ayd2.imporcomgua.dto.client.NewBusinessRequestDTO;
+import com.ayd2.imporcomgua.dto.client.UpdateBusinessRequestDTO;
+import com.ayd2.imporcomgua.exceptions.NotFoundException;
 import com.ayd2.imporcomgua.services.client.BusinessService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -25,5 +35,27 @@ public class BusinessController {
         final List<BusinessResponseDTO> business = businessService.getAllBusiness();
         return ResponseEntity.status(HttpStatus.OK).body(business);
     }
+
+    @PostMapping
+    public ResponseEntity<BusinessResponseDTO> createBusiness(
+            @RequestBody @Valid NewBusinessRequestDTO newBusinessRequestDTO) {
+        final BusinessResponseDTO business = businessService.createBusiness(newBusinessRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(business);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<BusinessResponseDTO> updateBusiness(
+            @PathVariable UUID id,
+            @RequestBody @Valid UpdateBusinessRequestDTO updateBusinessRequestDTO)
+            throws NotFoundException {
+        final BusinessResponseDTO business = businessService.updateBusiness(id, updateBusinessRequestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(business);
+    }
     
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBusiness(@PathVariable UUID id) throws NotFoundException {
+        businessService.deleteBusiness(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
 }
--- backend/src/main/java/com/ayd2/imporcomgua/services/client/BusinessServiceImpl.java
+++ backend/src/main/java/com/ayd2/imporcomgua/services/client/BusinessServiceImpl.java
@@ -1,12 +1,17 @@
 package com.ayd2.imporcomgua.services.client;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ayd2.imporcomgua.dto.client.BusinessResponseDTO;
+import com.ayd2.imporcomgua.dto.client.NewBusinessRequestDTO;
+import com.ayd2.imporcomgua.dto.client.UpdateBusinessRequestDTO;
+import com.ayd2.imporcomgua.exceptions.NotFoundException;
 import com.ayd2.imporcomgua.mappers.client.BusinessMapper;
+import com.ayd2.imporcomgua.models.client.Business;
 import com.ayd2.imporcomgua.repositories.client.BusinessRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -26,6 +31,31 @@ public class BusinessServiceImpl implements BusinessService {
                 .map(businessMapper::toBusinessResponseDTO)
                 .toList();
         return business;
+    }
+
+    @Override
+    public BusinessResponseDTO createBusiness(NewBusinessRequestDTO newBusinessRequestDTO) {
+        final Business business = businessMapper.toBusiness(newBusinessRequestDTO);
+        final Business newBusiness = businessRepository.save(business);
+        return businessMapper.toBusinessResponseDTO(newBusiness);
+    }
+
+    @Override
+    public BusinessResponseDTO updateBusiness(UUID id, UpdateBusinessRequestDTO updateBusinessRequestDTO)
+            throws NotFoundException {
+        final Business business = businessRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("No existe el negocio con id: " + id));
+        businessMapper.UpdateBusinessFromDTO(updateBusinessRequestDTO, business);
+        final Business updatedBusiness = businessRepository.save(business);
+        return businessMapper.toBusinessResponseDTO(updatedBusiness);
+    }
+
+    @Override
+    public void deleteBusiness(UUID id) throws NotFoundException {
+        final Business business = businessRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("No existe el negocio con id: " + id));
+        business.setIsActive(false);
+        businessRepository.save(business);
     }
     
 }
