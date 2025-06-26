ba65af87424e21b458f548c847ebbc28438707e2
LuisCif-20 <n.rodas225@gmail.com>
2025-06-26 00:17:34 -0600

    fix: update authentication

--- backend/src/main/java/com/ayd2/imporcomgua/config/SecurityConfig.java
+++ backend/src/main/java/com/ayd2/imporcomgua/config/SecurityConfig.java
@@ -67,7 +67,7 @@ public class SecurityConfig {
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .requestMatchers("/v3/api-docs*/**").permitAll()
-                        .anyRequest().authenticated())
+                        .anyRequest().permitAll())
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
