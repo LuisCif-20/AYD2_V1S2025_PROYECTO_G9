e972e9f3d00c7e8584e6b4434cd1ae9fb6dd8995
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-06-26 20:27:57 -0600

    fix: unique name business

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -12,7 +12,7 @@ CREATE TABLE municipality (
 
 CREATE TABLE business (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL UNIQUE,
     is_active BOOLEAN DEFAULT TRUE
 );
 
