b25e50dce7910685bffda7834a6c5c0888c7fc5d
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-06-27 23:14:53 -0600

    chore: add test format

---
+++ backend/src/test/java/com/ayd2/imporcomgua/services/client/BusinessServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.ayd2.imporcomgua.services.client;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BusinessServiceImplTest {
+    @Test
+    void doSomeTest() {
+        // Implement your test logic here
+    }
+}
